140ce6762f018c47ae2a6bd5eb6875d3e45ebe7c
Corey <corey@undimk3>
2013-11-25 22:22:48 -0500

    Added possible alternative to screen setup

--- src/space_base/ui/window.clj
+++ src/space_base/ui/window.clj
@@ -2,7 +2,7 @@
   (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
            [com.badlogic.gdx.graphics GL10]
            [com.badlogic.gdx.scenes.scene2d Stage]
-           [com.badlogic.gdx ApplicationListener Gdx]))
+           [com.badlogic.gdx ApplicationListener Gdx Screen]))
 
 (set! *warn-on-reflection* true)
 
@@ -16,6 +16,27 @@
                        ~@body))))
 
 (def stage (atom nil))
+
+;;The screen proxy and setScreen give
+;;  delta on render, but probably not needed with
+;;  how I'm going to implement physics/updates
+(defn game-screen
+  []
+  (proxy [Screen] []
+    (show []
+      (.. Gdx gl (glClearColor 0.2 0.5 1.0 0.0))
+      (.. Gdx gl10 (glViewport (int 0) (int 0) WIDTH HEIGHT))
+      (reset! stage (Stage. WIDTH HEIGHT)))
+    (render [delta]
+      (.. Gdx gl (glClear GL10/GL_COLOR_BUFFER_BIT))
+      (doto ^Stage @stage
+            (.act)
+            (.draw)))
+    (dispose [])
+    (hide [])
+    (pause [])
+    (resize [w h])
+    (resume [])))
 
 (deftype SpaceBase []
     ApplicationListener
