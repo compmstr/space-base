82e4f3f26fa6ebd12f22a388134d75f0121b0221
Corey <corey@undimk3>
2013-11-25 22:10:32 -0500

    Moved from lwjgl to libgdx

--- project.clj
+++ project.clj
@@ -3,10 +3,15 @@
   :url "http://example.com/FIXME"
   :soruce-path "src"
   :main space-base.core
+  :repositories [["sonatype"
+                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;Add src/natives to the path where java looks up native libs
-  :jvm-opts [~(str "-Djava.library.path=src/natives:"
-                   (System/getProperty "java.library.path"))]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.lwjgl.lwjgl/lwjgl "2.8.5"]])
+  ;;:jvm-opts [~(str "-Djava.library.path=src/natives:"
+  ;;                 (System/getProperty "java.library.path"))]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.badlogicgames.gdx/gdx "1.0-SNAPSHOT"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0-SNAPSHOT"]
+                 [com.badlogicgames.gdx/gdx-platform "1.0-SNAPSHOT"
+                  :classifier "natives-desktop"]])
--- src/space_base/ui/window.clj
+++ src/space_base/ui/window.clj
@@ -1,13 +1,41 @@
 (ns space-base.ui.window
-  (:import [org.lwjgl.opengl Display DisplayMode]))
+  (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
+           [com.badlogic.gdx.graphics GL10]
+           [com.badlogic.gdx.scenes.scene2d Stage]
+           [com.badlogic.gdx ApplicationListener Gdx]))
+
+(set! *warn-on-reflection* true)
+
+(def ^:const WIDTH (int 800))
+(def ^:const HEIGHT (int 600))
+
+(defmacro on-render-thread
+  [& body]
+  `(.. Gdx app
+       (postRunnable (fn []
+                       ~@body))))
+
+(def stage (atom nil))
+
+(deftype SpaceBase []
+    ApplicationListener
+  (create [this]
+    (.. Gdx gl (glClearColor 0.2 0.5 1.0 0.0))
+    (.. Gdx gl10 (glViewport (int 0) (int 0) WIDTH HEIGHT))
+    (reset! stage (Stage. WIDTH HEIGHT)))
+  (dispose [this])
+  (pause [this])
+  (render [this]
+    (.. Gdx gl (glClear GL10/GL_COLOR_BUFFER_BIT))
+    (doto ^Stage @stage
+      (.act)
+      (.draw)))
+  (resize [this w h])
+  (resume [this]))
 
 (defn start []
-  (Display/setDisplayMode (DisplayMode. 800 600))
-  (Display/create)
-  (loop []
-    (if (Display/isCloseRequested)
-      (println "Exiting")
-      (do
-        (Display/update)
-        (recur))))
-  (Display/destroy))
+  (LwjglApplication.
+   (SpaceBase.)
+   "Space Base"
+   WIDTH HEIGHT
+   false))
